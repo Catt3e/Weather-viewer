cf3584f937b1cf7a022baf77f0ab8883553255d3
Phạm Nguyễn Phước Thiện <135534799+Catt3e@users.noreply.github.com>
2026-04-13 17:49:26 +0700

    Chore: add city recommendation

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,46 +1,99 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+import { getCitySuggestions } from '../services/weatherAPI';
 
 function SearchBar({ onSearch }) {
     const [inputValue, setInputValue] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
+    const [showSuggestions, setShowSuggestions] = useState(false);
+    const debounceTimer = useRef(null);
 
     function handleSearch() {
         if (inputValue.trim() === '') return;
         onSearch(inputValue);
     }
 
+    function fetchSuggestions(query) {
+        if (query.trim() !== '') {
+            if (query.includes(' '))
+                query.replace(' ', '%20')
+            getCitySuggestions(query)
+                .then(data => {
+                    setSuggestions(data);
+                })
+                .catch(error => {
+                    console.error('Error fetching city suggestions:', error);
+                    setSuggestions([]);
+                });
+            setShowSuggestions(true);
+        } else {
+            setSuggestions([]);
+            setShowSuggestions(false);
+            return;
+        }
+    }
+
+    function handleInputChange(e) {
+        const value = e.target.value;
+        setInputValue(value);
+
+        if (debounceTimer.current)
+            clearTimeout(debounceTimer.current);
+        debounceTimer.current = setTimeout(() => {
+            fetchSuggestions(value);
+        }, 300);
+    }
+
     return (
         <div className="w-full max-w-sm mx-auto">
-            <input
-                type="text"
-                className="
-                    w-full
-                    bg-gray-900
-                    text-gray-100
-                    placeholder-gray-500
-                    border border-gray-800
-                    rounded-xl
-                    px-4 py-2
-                    outline-none
-                    focus:ring-2 focus:ring-blue-500
-                    focus:border-blue-500
-                    transition
-                "
-                placeholder="Search city..."
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-                onKeyDown={(e) => {
-                    if (e.key === 'Enter') {
-                        handleSearch();
-                    }
-                }}
-            />
-            <button
-                className="mt-2 w-full bg-blue-600 text-white py-2 rounded-xl hover:bg-blue-700 transition"
-                onClick={handleSearch}
-            >
-                Search
-            </button>
+            <div className="flex gap-2">
+                <input
+                    type="text"
+                    className="
+                        w-full
+                        bg-gray-900
+                        text-gray-100
+                        placeholder-gray-500
+                        border border-gray-800
+                        rounded-xl
+                        px-4 py-2
+                        outline-none
+                        focus:ring-2 focus:ring-blue-500
+                        focus:border-blue-500
+                        transition
+                    "
+                    placeholder="Search city..."
+                    value={inputValue}
+                    onChange={(e) => handleInputChange(e)}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            handleSearch();
+                        }
+                    }}
+                />
+                <button
+                    className="mt-2 w-full bg-blue-600 text-white py-2 rounded-xl hover:bg-blue-700 transition"
+                    onClick={handleSearch}
+                >
+                    Search
+                </button>
+            </div>
+            <ul className="mt-2 bg-gray-900 border border-gray-800 rounded-xl max-h-48 overflow-y-auto">
+                {showSuggestions && suggestions.map((suggestion, index) => (
+                    <li
+                        key={index}
+                        className="bg-gray-800 text-gray-300 hover:bg-gray-600 cursor-pointer"
+                        onClick={() => {
+                            setInputValue(suggestion.name);
+                            onSearch(suggestion.name);
+                            setShowSuggestions(false);
+                        }}
+                    >
+                        {suggestion.name}, {suggestion.country}
+                    </li>
+                ))}
+            </ul>
         </div>
+        
     );
 }
 
--- src/services/weatherAPI.js
+++ src/services/weatherAPI.js
@@ -20,9 +20,8 @@ async function getForecast(city) {
     const url = `${BASE_URL}forecast?q=${city}&units=metric&appid=${API_KEY}`;
     try {
         const response = await fetch(url);
-        if (!response.ok) {
+        if (!response.ok)
             throw new Error(`Error ${response.status}: ${response.statusText} - ${city} not found`);
-        }
         const data = await response.json();
         return data;
     } catch (error) {
@@ -31,4 +30,18 @@ async function getForecast(city) {
     }
 }
 
-export { getCurrentWeatherAPI, getForecast };
+async function getCitySuggestions(query) {
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok)
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching city suggestions:", error);
+        throw error;
+    }
+}
+
+export { getCurrentWeatherAPI, getForecast, getCitySuggestions };
